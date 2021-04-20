264c63a6c58b8d0f3f8e647b5dbb69dc2168e9b8
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-04-20 23:50:07 +0300

    tree study

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 My LeetCode Solutions
 
 # solved with help
-[415. Add Strings](https://leetcode.com/problems/add-strings/)
-[989. Add to Array-Form of Integer](https://leetcode.com/problems/add-to-array-form-of-integer/)
+- [415. Add Strings](https://leetcode.com/problems/add-strings/)
+- [989. Add to Array-Form of Integer](https://leetcode.com/problems/add-to-array-form-of-integer/)
---
+++ tree.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+type NodeTree struct {
+	Key   int
+	Value string
+	Left  *NodeTree
+	Right *NodeTree
+}
+
+func Find(root *NodeTree, needle int) *string {
+	// find in sorted tree
+	if root == nil {
+		return nil
+	}
+
+	if needle == root.Key {
+		return &root.Value
+	} else if needle > root.Key {
+		return Find(root.Right, needle)
+	} else {
+		return Find(root.Left, needle)
+	}
+}
+
+func Add(root, new *NodeTree) {
+	// find in sorted tree
+	if root == nil {
+		root = new
+	}
+
+	if new.Key == root.Key {
+		root.Value = new.Value
+		return
+	} else if new.Key > root.Key {
+		if root.Right != nil {
+			Add(root.Right, new)
+		} else {
+			root.Right = new
+		}
+	} else {
+		if root.Left != nil {
+			Add(root.Left, new)
+		} else {
+			root.Left = new
+		}
+	}
+}
+
+func PrintTree(root *NodeTree) {
+	if root == nil {
+		return
+	}
+
+	PrintTree(root.Left)
+	fmt.Print(root.Value)
+	fmt.Print("-")
+	PrintTree(root.Right)
+}
+
+func main() {
+	//	      10
+	//     /     \
+	//    5       35
+	//   /      /    \
+	//  1      20    99
+	//   \    /  \
+	//    4  17  31
+	tree := NodeTree{
+		10,
+		"root",
+		&NodeTree{
+			5,
+			"root5",
+			&NodeTree{1, "root1", nil, &NodeTree{4, "root4", nil, nil}},
+			nil,
+		},
+		&NodeTree{
+			35,
+			"root35",
+			&NodeTree{20, "root20", &NodeTree{17, "root17", nil, nil}, &NodeTree{31, "root31", nil, nil}},
+			&NodeTree{99, "root99", nil, nil},
+		},
+	}
+
+	//fmt.Print(*Find(&tree, 20))
+	Add(&tree, &NodeTree{5, "doot5", nil, nil})
+	PrintTree(&tree)
+}
