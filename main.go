c5e611223a232c4313cb7dee706038ab0ed2d6df
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-04-20 19:16:17 +0300

    _415_Add_Strings

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import . "./415_Add_Strings"
+
+func main() {
+	fmt.Print(
+		AddStrings("0", "0"),
+	)
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+My LeetCode Solutions
+
+# solved with help
+[415. Add Strings](https://leetcode.com/problems/add-strings/)
