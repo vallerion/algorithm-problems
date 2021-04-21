25559368def2274d6e45e587e7f7a8fc611fbb54
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-04-21 09:49:32 +0300

    _17_Merge_Two_Binary_Trees

---
+++ 617_Merge_Two_Binary_Trees/solution.go
@@ -0,0 +1,29 @@
+package _17_Merge_Two_Binary_Trees
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	} else if root2 == nil {
+		return root1
+	} else {
+		root1.Val += root2.Val
+		root1.Left = mergeTrees(root1.Left, root2.Left)
+		root1.Right = mergeTrees(root1.Right, root2.Right)
+	}
+
+	return root1
+}
