1fa9b82870a2236584b1240744fbf8c20cc64ac6
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-05-18 09:33:07 +0300

    dsu

--- lessons/problems/building_min_heap.go
+++ lessons/problems/building_min_heap.go
@@ -1,4 +1,4 @@
-package lessons
+package problems
 
 import (
 	"bufio"
--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-//import . "./cormen/exercises"
-import . "./leetcode/may_contest"
+import . "./lessons"
 
 func main() {
-	//fmt.Println(IsPrime(100000000))
-	//fmt.Println(IsPrime2(math.MaxInt32))
-	//fmt.Println(IsPrime3(math.MaxInt32))
-	fmt.Println(CountPrimes(100000))
+	MainDSU()
 }
