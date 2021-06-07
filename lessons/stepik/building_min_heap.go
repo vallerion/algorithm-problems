f518aa1b473f40ee777bf1a9ade8ae6865808080
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-06-07 13:28:28 +0300

    add tree height task

--- lessons/stepik/building_min_heap.go
+++ lessons/stepik/building_min_heap.go
@@ -1,4 +1,4 @@
-package problems
+package stepik
 
 import (
 	"bufio"
---
+++ lessons/stepik/tree_height.go
@@ -0,0 +1,49 @@
+package stepik
+
+import "fmt"
+import "strconv"
+import "strings"
+import "bufio"
+import "os"
+
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+
+	in.ReadString('\n')
+	input, _ := in.ReadString('\n')
+	input = strings.TrimRight(input, "\n")
+
+	var root int
+	tb := make(map[int][]int, 0)
+
+	for i, v := range strings.Split(input, " ") {
+		node, _ := strconv.Atoi(v)
+		if node == -1 {
+			root = i
+			continue
+		}
+		tb[node] = append(tb[node], i)
+	}
+
+	queue := tb[root]
+	height := 0
+
+	// fmt.Println(tb)
+	// fmt.Println(queue)
+
+	for len(queue) > 0 {
+		curr := queue
+		queue = []int{}
+
+		for _, v := range curr {
+			if _, ok := tb[v]; ok {
+				queue = append(queue, tb[v]...)
+			}
+		}
+
+		height++
+	}
+
+	fmt.Println(height+1)
+}
