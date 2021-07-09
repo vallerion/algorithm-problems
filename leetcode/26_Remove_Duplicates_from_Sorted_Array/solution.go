7b2460b479e8122c557a444faa20376fd94754e8
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-07-09 09:59:20 +0300

    solve again _26_Remove_Duplicates_from_Sorted_Array

--- leetcode/26_Remove_Duplicates_from_Sorted_Array/solution.go
+++ leetcode/26_Remove_Duplicates_from_Sorted_Array/solution.go
@@ -1,8 +1,21 @@
 package _26_Remove_Duplicates_from_Sorted_Array
 
 // [0,0,1,1,1,2,2,3,3,4]
-// ...
+// j = 1, i = 0 -> nums[j-1] != nums[i] { nums[j] = nums[i] j++ } -> 0 != 0 -> false
+// j = 1, i = 1 -> nums[j-1] != nums[i] { nums[j] = nums[i] j++ } -> 0 != 0 -> false
+// j = 1, i = 2 -> nums[j-1] != nums[i] { nums[j] = nums[i] j++ } -> 0 != 1 -> true
 // [0,1,1,1,1,2,2,3,3,4]
+// j = 2, i = 3 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 1 != 1 -> false
+// j = 2, i = 4 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 1 != 1 -> false
+// j = 2, i = 5 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 1 != 2 -> true
+// [0,1,2,1,1,2,2,3,3,4]
+// j = 3, i = 6 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 2 != 2 -> false
+// j = 3, i = 7 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 2 != 3 -> true
+// [0,1,2,3,1,2,2,3,3,4]
+// j = 4, i = 8 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 3 != 3 -> false
+// j = 4, i = 9 -> nums[j-1] != nums[i] { nums[j] = nums[i] } -> 3 != 4 -> true
+// [0,1,2,3,4,2,2,3,3,4]
+
 func removeDuplicates(nums []int) int {
 	if len(nums) <= 1 {
 		return len(nums)
@@ -18,30 +31,4 @@ func removeDuplicates(nums []int) int {
 	}
 
 	return j
-}
-
-func removeDuplicatesSlow(nums []int) int {
-	counter := 0
-
-	for i := 1; i < len(nums); i++ {
-		if counter >= len(nums)-i {
-			break
-		}
-
-		if nums[i] == nums[i-1] {
-			moveToEnd(&nums, i)
-			counter++
-			i--
-		}
-	}
-
-	return len(nums) - counter
-}
-
-func moveToEnd(nums *[]int, index int) {
-	for i := index; i < len(*nums)-1; i++ {
-		temp := (*nums)[i]
-		(*nums)[i] = (*nums)[i+1]
-		(*nums)[i+1] = temp
-	}
 }
