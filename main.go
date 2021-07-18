86079f0926eb48a89873b137865bc88ac3c6c98f
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-07-18 16:28:03 +0300

    delete olds

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-type IntValue int
+// a a b a a b a a a a b  a  a  b  а  a  a  b
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+// 0 0 0 0 0 0 0 0 0 0 0  0  0  0  0  0  0  0
+// _ _
 
-func (value *IntValue) compare(input *TreeValue) int {
-	//inputInt := input.(IntValue)
-}
+// _ _ -> i=1, j=i-1
+// 0 1 0 0 0 0 0 0 0 0 0  0  0  0  0  0  0  0
+
+// i=2,j=1 (j=p[2-1]=p[1]=1)
 
 func main() {
-	tree := AVLTree{&treeNode{}}
+	//ыва
+}
+
+func prefix(str string) []int {
+	s := []rune(str)
+	prefixes := make([]int, len(str))
+
+	for i := 1; i < len(str); i++ {
+		j := prefixes[i-1]
+
+		if s[i] == s[j] {
+			j++
+		}
+
+		prefixes[i] = j
+	}
+
+	return prefixes
 }
