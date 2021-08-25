923cd50b6ffc9af9b2c302815099c3dd940783dc
valeriy.nezhuriov <valeriy.nezhuriov@gmail.com>
2021-08-25 16:38:39 +0300

    interative _230_Kth_Smallest_Element_in_a_BST

--- leetcode/230_Kth_Smallest_Element_in_a_BST/solution.go
+++ leetcode/230_Kth_Smallest_Element_in_a_BST/solution.go
@@ -7,6 +7,24 @@ type TreeNode struct {
 }
 
 func kthSmallest(root *TreeNode, k int) int {
+	stack := make([]*TreeNode, 0)
+
+	for {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
+}
+
+func kthSmallestTreeToArray(root *TreeNode, k int) int {
 	arr := make([]int, 0)
 	treeToArray(root, &arr)
 	return arr[k-1]
