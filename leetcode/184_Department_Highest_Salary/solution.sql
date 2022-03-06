9328dc729995369a9727d0f73e1113ca6167a634
valeriy nezhurov <valeriy.nezhuriov@gmail.com>
2022-03-06 20:47:26 +0200

    184_Department_Highest_Salary

---
+++ leetcode/184_Department_Highest_Salary/solution.sql
@@ -0,0 +1,38 @@
+/*
+Input:
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output:
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+ */
+
+select
+    Department.name as Department, Employee.name as Employee, Employee.salary as Salary
+from Employee
+         join (select max(Employee.salary) as salary,
+                      Employee.departmentId
+               from Employee
+               group by Employee.departmentId) as emax
+              on emax.salary = Employee.salary and emax.departmentId = Employee.departmentId
+join Department on Department.id = Employee.departmentId
