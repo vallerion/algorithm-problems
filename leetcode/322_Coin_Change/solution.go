a120234ed0f1123d5d4b7035b01b44dccb2cbac1
valeriy nezhurov <valeriy.nezhuriov@gmail.com>
2022-05-21 10:06:22 +0300

    _322_Coin_Change

--- leetcode/322_Coin_Change/solution.go
+++ leetcode/322_Coin_Change/solution.go
@@ -1,43 +1,49 @@
 package _322_Coin_Change
 
-import "math"
+// [1,2,5]
+// 11
 
-// coins = [1,2,5], amount = 11
-// [0,0,0,0,0,0,0,0,0,0,0]
-// [0,1,0,0,0,0,0,0,0,0,0]
-// [0,1,1,2,2,0,0,0,0,0,0]
-
-// 1 -> 1
-// 2 -> 1
-// 3 -> 2
-// 4 -> 2
+// [1,1,2,2,1,2,2,3,3, 2, 3]
+//  1 2 3 4 5 6 7 8 9 10 11
 
 func coinChange(coins []int, amount int) int {
-	changes := make([]int, amount+1)
-
-	for i := 1; i <= amount; i++ {
-		smallest := math.MaxInt32
-		for j := 0; j < len(coins); j++ {
-			if coins[j] < i {
-				smallest = min(smallest, changes[i-coins[j]]+1)
-			} else if coins[j] == i {
-				smallest = 1
-			}
-		}
-
-		changes[i] = smallest
+	if amount == 0 {
+		return 0
 	}
 
-	if changes[amount] == math.MaxInt32 {
+	dp := make([]int, amount)
+	curr := 0
+
+	for curr < amount {
+		for i := 0; i < len(coins); i++ {
+			if curr+1 == coins[i] {
+				dp[curr] = 1
+				break
+			}
+			if curr+1 < coins[i] || dp[curr-coins[i]] == 0 {
+				continue
+			}
+
+			if dp[curr] == 0 {
+				dp[curr] = dp[curr-coins[i]] + 1
+			} else {
+				dp[curr] = min(dp[curr], dp[curr-coins[i]]+1)
+			}
+		}
+		curr++
+	}
+
+	if dp[amount-1] == 0 {
 		return -1
 	}
 
-	return changes[amount]
+	return dp[amount-1]
 }
 
 func min(a, b int) int {
 	if a < b {
 		return a
 	}
+
 	return b
 }
