991f216e72d18a64a342d694ee0916542d85e86d
valeriy nezhurov <valeriy.nezhuriov@gmail.com>
2022-12-12 11:08:20 +0100

    _70_Climbing_Stairs

--- leetcode/70_Climbing_Stairs/solution.go
+++ leetcode/70_Climbing_Stairs/solution.go
@@ -1,29 +1,19 @@
 package _70_Climbing_Stairs
 
+var cache map[int]int
+
 func climbStairs(n int) int {
-	if n == 1 {
-		return 1
+	if cache == nil {
+		cache = make(map[int]int)
+	}
+	if n <= 2 {
+		return n
+	}
+	if _, ok := cache[n]; ok {
+		return cache[n]
 	}
 
-	steps := make([]int, n)
+	cache[n] = climbStairs(n-1) + climbStairs(n-2)
 
-	steps[0] = 1
-	steps[1] = 2
-
-	for i := 2; i < n; i++ {
-		steps[i] = steps[i-1] + steps[i-2]
-	}
-
-	return steps[n-1]
-}
-
-func climbStairs_Recursive(n int) int {
-	if n < 0 {
-		return 0
-	}
-	if n == 0 {
-		return 1
-	}
-
-	return climbStairs(n-2) + climbStairs(n-1)
+	return cache[n]
 }
